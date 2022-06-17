9da56cb94bca6eb6c0124013e49a473f71a6f9e2
Kyle Dilick <kdilick@gmail.com>
2022-06-17 09:51:58 -0600

    card seed file finished

--- client/src/routes/CardGallery.jsx
+++ client/src/routes/CardGallery.jsx
@@ -4,32 +4,51 @@ import "../_styled/CardGallery.css";
 
 const cardPath = "/assets/cards/";
 const fileNames = [
-  "(DFAC)  Burn Water.png",
-  "(DFAC)  No PT Gear.png",
-  "(DFAC)  Ridiculously Sharp Knife.png",
-  "(DFAC)  Undercooked Chicken.png",
-  "(Finance)  Ignore Calls & E-mails.png",
-  "(Finance)  Mess Up Member Pay.png",
-  "(Finance)  Misfiled Travel Voucher.png",
-  "(Finance)  Schrodinger's Office Hours.png",
-  "(General)  Antidepressants.png",
-  "(General)  BX Haircut.png",
-  "(General)  Commander's Call.png",
-  "(General)  Dirtbag Airman.png",
-  "(General)  Pull 341.png",
-  "(General)  Receive Article 15.png",
-  "(General)  Studying the Blade.png",
-  "(General)  Take Leave.png",
-  "(General)  VItamin M.png",
-  "(General)  You Are The 1 Percent.png",
-  "(SecFo)  CAC Check.png",
-  "(SecFo)  Dorm Raid.png",
-  "(SecFo)  Expired Plates.png",
-  "(SecFo)  Vehicle Search.png",
-  "(SpecOps)  AOR Beard.png",
-  "(SpecOps)  Overwhelming Firepower.png",
-  "(SpecOps)  This Isn't Even My Final Form.png",
-  "(SpecOps)  Write Tell-All Book.png",
+    "(DFAC) Burn Water.png",
+    "(DFAC)  No PT Gear.png",
+    "(DFAC)  Ridiculously Sharp Knife.png",
+    "(DFAC)  Undercooked Chicken.png",
+    "(DFAC)  Turducken.png",
+    "(Finance)  Ignore Calls & E-mails.png",
+    "(Finance)  Mess Up Member Pay.png",
+    "(Finance)  Misfiled Travel Voucher.png",
+    "(Finance)  Overpay.png",
+    "(Finance)  Schrodinger's Office Hours.png",
+    "(General)  Antidepressants.png",
+    "(General)  BX Haircut.png",
+    "(General)  Commander's Call.png",
+    "(General)  Dirtbag Airman.png",
+    "(General)  Healthy Coping Mechanisms.png",
+    "(General)  Pay With GTC.png",
+    "(General)  Pull 341.png",
+    "(General)  Receive Article 15.png",
+    "(General)  Reenlist.png",
+    "(General)  Referred To Mental Health.png",
+    "(General)  Refreshing Energy Drink.png",
+    "(General)  Snuffy The Green.png",
+    "(General)  Special Duty Sorting Hat.png",
+    "(General)  Studying the Blade.png",
+    "(General)  Take Leave.png",
+    "(General)  VItamin M.png",
+    "(General)  Well-Written Rebuttal.png",
+    "(General)  Yelled At By A Dependa.png",
+    "(General)  You Are The 1 Percent.png",
+    "(General)  Totally Baller MRE.png",
+    "(Intel)  Cuddle Waifu Bodypillow.png",
+    "(Intel)  I Talked To A Girl!.png",
+    "(Intel)  Send Opponent To Shadow Zone.png",
+    "(Intel)  World of Warcraft Raid Wipe.png",
+    "(Intel)%20%20Avoid%20The%20Sun.png",
+    "(SecFo)  CAC Check.png",
+    "(SecFo)  Dorm Raid.png",
+    "(SecFo)  Expired Plates.png",
+    "(SecFo)  Forced Augmentee.png",
+    "(SecFo)  Vehicle Search.png",
+    "(SpecOps)  AOR Beard.png",
+    "(SpecOps)  Decepticon Attack!.png",
+    "(SpecOps)  Overwhelming Firepower.png",
+    "(SpecOps)  This Isn't Even My Final Form.png",
+    "(SpecOps)  Write Tell-All Book.png"
 ];
 
 const CardGallery = () => {
@@ -50,16 +69,13 @@ const CardGallery = () => {
     <div className="card-gallery-container">
       <div className="card-gallery">
         {allCards.map((card) => {
-          console.log(card.name);
-          console.log("(DFAC)  Undercooked Chicken");
-          if (card.name === "(DFAC)  Undercooked Chicken") {
-            console.log("match");
-          }
+          let filePath = cardPath + card.name + ".png"
           return (
             <img
               className="card"
+              // src={"/assets/cards/SpecOps_Write_Tell-All_Book.png"}
               src={cardPath + card.name + ".png"}
-              alt={cardPath + card.name + ".png"}
+              alt={filePath}
             />
           );
         })}
--- server/backup/CardData.js
+++ server/backup/CardData.js
@@ -7,6 +7,7 @@ export async function seed(knex) {
   await knex("cards").del();
   await knex("cards").insert([
     {
+      id: 1,
       name: "DFAC_Burn_Water",
       description: '"It\'s one part Cuyahoga River, one part incompetence. "',
       background_img: "",
@@ -21,6 +22,7 @@ export async function seed(knex) {
       temp_effect: "false",
     },
     {
+      id: 2,
       name: "DFAC_No_PT_Gear",
       description: '"You need to go change."',
       background_img: "",
@@ -35,6 +37,7 @@ export async function seed(knex) {
       temp_effect: "false",
     },
     {
+      id: 3,
       name: "DFAC_Ridiculously_Sharp_Knife",
       description: '"When you were partying, "',
       background_img: "",
@@ -49,6 +52,7 @@ export async function seed(knex) {
       temp_effect: "false",
     },
     {
+      id: 4,
       name: "DFAC_Turducken",
       description:
         '"It\'s an abomination. A delicious succulent, savory abomination."',
@@ -64,6 +68,7 @@ export async function seed(knex) {
       temp_effect: "false",
     },
     {
+      id: 5,
       name: "DFAC_Undercooked_Chicken",
       description: '""',
       background_img: "",
@@ -78,6 +83,7 @@ export async function seed(knex) {
       temp_effect: "false",
     },
     {
+      id: 6,
       name: "Finance_Ignore_Calls_Emails",
       description: '""',
       background_img: "",
@@ -92,6 +98,7 @@ export async function seed(knex) {
       temp_effect: "false",
     },
     {
+      id: 7,
       name: "Finance_Mess_Up_Member_Pay",
       description: '""',
       background_img: "",
@@ -106,6 +113,7 @@ export async function seed(knex) {
       temp_effect: "false",
     },
     {
+      id: 8,
       name: "Finance_Misfiled_Travel_Voucher",
       description: '""',
       background_img: "",
@@ -120,6 +128,7 @@ export async function seed(knex) {
       temp_effect: "false",
     },
     {
+      id: 9,
       name: "Finance_Overpay",
       description: '""',
       background_img: "",
@@ -134,6 +143,7 @@ export async function seed(knex) {
       temp_effect: "false",
     },
     {
+      id: 10,
       name: "Finance_Schrodingers_Office_Hours",
       description: '""',
       background_img: "",
@@ -148,6 +158,7 @@ export async function seed(knex) {
       temp_effect: "false",
     },
     {
+      id: 11,
       name: "General_Antidepressants",
       description: '""',
       background_img: "",
@@ -162,6 +173,7 @@ export async function seed(knex) {
       temp_effect: "false",
     },
     {
+      id: 12,
       name: "General_BX_Haircut",
       description: '""',
       background_img: "",
@@ -176,6 +188,7 @@ export async function seed(knex) {
       temp_effect: "false",
     },
     {
+      id: 13,
       name: "General_Commanders_Call",
       description: '""',
       background_img: "",
@@ -190,6 +203,7 @@ export async function seed(knex) {
       temp_effect: "false",
     },
     {
+      id: 14,
       name: "General_Dirtbag_Airman",
       description: '""',
       background_img: "",
@@ -204,6 +218,7 @@ export async function seed(knex) {
       temp_effect: "false",
     },
     {
+      id: 15,
       name: "General_Healthy_Coping_Mechanisms",
       description: '""',
       background_img: "",
@@ -218,6 +233,7 @@ export async function seed(knex) {
       temp_effect: "false",
     },
     {
+      id: 16,
       name: "General_Pay_With_GTC",
       description: '""',
       background_img: "",
@@ -232,6 +248,7 @@ export async function seed(knex) {
       temp_effect: "false",
     },
     {
+      id: 17,
       name: "General_Pull_341",
       description: '""',
       background_img: "",
@@ -246,6 +263,7 @@ export async function seed(knex) {
       temp_effect: "false",
     },
     {
+      id: 18,
       name: "General_Receive_Article_15",
       description: '""',
       background_img: "",
@@ -260,6 +278,7 @@ export async function seed(knex) {
       temp_effect: "false",
     },
     {
+      id: 19,
       name: "General_Reenlist",
       description: '""',
       background_img: "",
@@ -274,6 +293,7 @@ export async function seed(knex) {
       temp_effect: "false",
     },
     {
+      id: 20,
       name: "General_Referred_To_Mental_Health",
       description: '""',
       background_img: "",
@@ -288,6 +308,7 @@ export async function seed(knex) {
       temp_effect: "false",
     },
     {
+      id: 21,
       name: "General_Refreshing_Energy_Drink",
       description: '""',
       background_img: "",
@@ -302,6 +323,7 @@ export async function seed(knex) {
       temp_effect: "false",
     },
     {
+      id: 22,
       name: "General_Snuffy_The_Green",
       description: '""',
       background_img: "",
@@ -316,6 +338,7 @@ export async function seed(knex) {
       temp_effect: "false",
     },
     {
+      id: 23,
       name: "General_Special_Duty_Sorting_Hat",
       description: '""',
       background_img: "",
@@ -330,6 +353,7 @@ export async function seed(knex) {
       temp_effect: "false",
     },
     {
+      id: 24,
       name: "General_Studying_The_Blade",
       description: '""',
       background_img: "",
@@ -344,6 +368,7 @@ export async function seed(knex) {
       temp_effect: "false",
     },
     {
+      id: 25,
       name: "General_Take_Leave",
       description: '""',
       background_img: "",
@@ -358,6 +383,7 @@ export async function seed(knex) {
       temp_effect: "false",
     },
     {
+      id: 26,
       name: "General_Vitamin_M",
       description: '""',
       background_img: "",
@@ -372,6 +398,7 @@ export async function seed(knex) {
       temp_effect: "false",
     },
     {
+      id: 27,
       name: "General_Well-Written_Rebuttal",
       description: '""',
       background_img: "",
@@ -386,6 +413,7 @@ export async function seed(knex) {
       temp_effect: "false",
     },
     {
+      id: 28,
       name: "General_Yelled_At_By_A_Dependa",
       description: '""',
       background_img: "",
@@ -400,6 +428,7 @@ export async function seed(knex) {
       temp_effect: "false",
     },
     {
+      id: 29,
       name: "General_You_Are_The_1_Percent",
       description: '""',
       background_img: "",
@@ -414,6 +443,7 @@ export async function seed(knex) {
       temp_effect: "false",
     },
     {
+      id: 30,
       name: "General_Totally_Baller_MRE",
       description: '""',
       background_img: "",
@@ -428,6 +458,7 @@ export async function seed(knex) {
       temp_effect: "false",
     },
     {
+      id: 31,
       name: "Intel_Avoid_The_Sun",
       description: '""',
       background_img: "",
@@ -442,6 +473,7 @@ export async function seed(knex) {
       temp_effect: "false",
     },
     {
+      id: 32,
       name: "Intel_Cuddle_Waifu_Bodypillow",
       description: '""',
       background_img: "",
@@ -456,6 +488,7 @@ export async function seed(knex) {
       temp_effect: "false",
     },
     {
+      id: 33,
       name: "Intel_I_Talked_To_A_Girl!",
       description: '""',
       background_img: "",
@@ -470,6 +503,7 @@ export async function seed(knex) {
       temp_effect: "false",
     },
     {
+      id: 34,
       name: "Intel_Send_Opponent_To_Shadow_Zone",
       description: '""',
       background_img: "",
@@ -484,6 +518,7 @@ export async function seed(knex) {
       temp_effect: "false",
     },
     {
+      id: 35,
       name: "Intel_World_Of_Warcraft_Raid_Wipe",
       description: '""',
       background_img: "",
@@ -498,6 +533,7 @@ export async function seed(knex) {
       temp_effect: "false",
     },
     {
+      id: 36,
       name: "SecFo_CAC_Check",
       description: '""',
       background_img: "",
@@ -512,6 +548,7 @@ export async function seed(knex) {
       temp_effect: "false",
     },
     {
+      id: 37,
       name: "SecFo_Dorm_Raid",
       description: '""',
       background_img: "",
@@ -526,6 +563,7 @@ export async function seed(knex) {
       temp_effect: "false",
     },
     {
+      id: 38,
       name: "SecFo_Expired_Plates",
       description: '""',
       background_img: "",
@@ -540,6 +578,7 @@ export async function seed(knex) {
       temp_effect: "false",
     },
     {
+      id: 39,
       name: "SecFo_Forced_Augmentee",
       description: '""',
       background_img: "",
@@ -554,6 +593,7 @@ export async function seed(knex) {
       temp_effect: "false",
     },
     {
+      id: 40,
       name: "SecFo_Vehicle_Search",
       description: '""',
       background_img: "",
@@ -568,6 +608,7 @@ export async function seed(knex) {
       temp_effect: "false",
     },
     {
+      id: 41,
       name: "SpecOps_AOR_Beard",
       description: '""',
       background_img: "",
@@ -582,6 +623,7 @@ export async function seed(knex) {
       temp_effect: "false",
     },
     {
+      id: 42,
       name: "SpecOps_Decepticon_Attack",
       description: '""',
       background_img: "",
@@ -596,6 +638,7 @@ export async function seed(knex) {
       temp_effect: "false",
     },
     {
+      id: 43,
       name: "SpecOps_Overwhelming_Firepower",
       description: '""',
       background_img: "",
@@ -610,6 +653,7 @@ export async function seed(knex) {
       temp_effect: "false",
     },
     {
+      id: 44,
       name: "SpecOps_This_Isnt_Even_My_Final_Form",
       description: '""',
       background_img: "",
@@ -624,6 +668,7 @@ export async function seed(knex) {
       temp_effect: "false",
     },
     {
+      id: 45,
       name: "SpecOps_Write_Tell-All_Book",
       description: '""',
       background_img: "",
